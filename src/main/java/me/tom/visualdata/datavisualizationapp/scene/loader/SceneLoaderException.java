dbb8fffdc68197391597e0cfe8d76d1a885f3352
tom <tpampiermole@gmail.com>
2024-03-08 15:37:18 +0100

    feat(scene): add SceneLoaderException to be thrown when a Scene could not be loaded

---
+++ src/main/java/me/tom/visualdata/datavisualizationapp/scene/loader/SceneLoaderException.java
@@ -0,0 +1,9 @@
+package me.tom.visualdata.datavisualizationapp.scene.loader;
+
+import me.tom.visualdata.datavisualizationapp.VisualizationAppException;
+
+public class SceneLoaderException extends VisualizationAppException {
+    protected SceneLoaderException(Exception exception) {
+        super(exception);
+    }
+}
