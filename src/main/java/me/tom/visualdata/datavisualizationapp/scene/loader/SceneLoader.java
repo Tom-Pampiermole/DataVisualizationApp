cce40fa609618ba726d5279eb3cfb568b803bf46
tom <tpampiermole@gmail.com>
2024-03-08 15:37:57 +0100

    feat(scene): add interface SceneLoader to be used for loading a Scene

---
+++ src/main/java/me/tom/visualdata/datavisualizationapp/scene/loader/SceneLoader.java
@@ -0,0 +1,19 @@
+package me.tom.visualdata.datavisualizationapp.scene.loader;
+
+import javafx.scene.Scene;
+
+import java.io.IOException;
+
+public interface SceneLoader {
+    /**
+     *  Loads a new {@link Scene} with given name and given size
+     *
+     * @param name Name from the scene to load
+     * @param width Width from the scene
+     * @param height Height from the scene
+     * @return Loaded {@link Scene}
+     *
+     * @throws IOException Thrown when scene could not be loaded
+     */
+    Scene load(String name, int width, int height) throws IOException;
+}
