8bc95566c0ce51de7e69f0425d7b8a735cf84015
tom <tpampiermole@gmail.com>
2024-03-08 15:58:28 +0100

    feat(scene): add enum to contain every Scene to be used

---
+++ src/main/java/me/tom/visualdata/datavisualizationapp/scene/AppScene.java
@@ -0,0 +1,41 @@
+package me.tom.visualdata.datavisualizationapp.scene;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import me.tom.visualdata.datavisualizationapp.scene.loader.FXMLSceneLoader;
+import me.tom.visualdata.datavisualizationapp.scene.loader.SceneLoader;
+
+public enum AppScene {
+    DASHBOARD("dashboard")
+    ;
+
+
+    private final Scene scene;
+    AppScene(String name) {
+        try {
+            scene = Defaults.SCENE_LOADER.load(name, Defaults.SCENE_WIDTH, Defaults.SCENE_HEIGHT);
+        } catch(Exception exception) {
+            throw new IllegalStateException(exception);
+        }
+    }
+
+    /**
+     *  Makes this scene the currently active scene from given {@link Stage}
+     *
+     * @param stage {@link Stage} to set the active scene of
+     */
+    void makeActive(Stage stage) {
+        assert stage != null : String.format("Cannot make %s active: stage null", getClass().getSimpleName());
+        stage.setScene(scene);
+    }
+
+
+    /**
+     *  Interface to contain default values from enum due to the inability of
+     */
+    private interface Defaults {
+        SceneLoader SCENE_LOADER = new FXMLSceneLoader();
+        int SCENE_WIDTH = 1200;
+        int SCENE_HEIGHT = 1000;
+    }
+}
