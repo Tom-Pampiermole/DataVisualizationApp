b96c844e6e2cdce39b4f78ea36bf93e1f943c57e
tom <tpampiermole@gmail.com>
2024-03-08 15:37:36 +0100

    perf(scene): make constructors of VisualizationAppException protected

--- src/main/java/me/tom/visualdata/datavisualizationapp/VisualizationAppException.java
+++ src/main/java/me/tom/visualdata/datavisualizationapp/VisualizationAppException.java
@@ -1,11 +1,11 @@
 package me.tom.visualdata.datavisualizationapp;
 
 public class VisualizationAppException extends Exception {
-    public VisualizationAppException(String message) {
+    protected VisualizationAppException(String message) {
         super(message);
     }
 
-    public VisualizationAppException(Exception exception) {
+    protected VisualizationAppException(Exception exception) {
         super(exception);
     }
 }
