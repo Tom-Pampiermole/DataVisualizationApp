b55f2621736276da73970d771ca784e04d4c0126
tom <tpampiermole@gmail.com>
2024-03-11 12:21:30 +0100

    feat(scene-component): make use of FXMLComponentLoader

--- src/main/java/me/tom/visualdata/scene/components/NavigationBar.java
+++ src/main/java/me/tom/visualdata/scene/components/NavigationBar.java
@@ -1,7 +1,16 @@
 package me.tom.visualdata.scene.components;
 
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import me.tom.visualdata.scene.components.loader.ComponentLoaderException;
+import me.tom.visualdata.scene.components.loader.FXMLComponentLoader;
 
-public class NavigationBar extends HBox {
+public class NavigationBar extends VBox {
+    private static final String COMPONENT_NAME = "NavigationBar";
 
+
+    public NavigationBar() throws ComponentLoaderException {
+//        TODO Make class
+        new FXMLComponentLoader().load(COMPONENT_NAME, this);
+
+    }
 }
