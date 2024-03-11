2dde7d7df0ae355762cebf6c3a094908d1dcaf44
tom <tpampiermole@gmail.com>
2024-03-11 12:48:53 +0100

    feat(scene-component): let VBox from component be height of parent

--- src/main/java/me/tom/visualdata/scene/components/NavigationBar.java
+++ src/main/java/me/tom/visualdata/scene/components/NavigationBar.java
@@ -1,16 +1,33 @@
 package me.tom.visualdata.scene.components;
 
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
-import me.tom.visualdata.scene.components.loader.ComponentLoaderException;
 import me.tom.visualdata.scene.components.loader.FXMLComponentLoader;
 
-public class NavigationBar extends VBox {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NavigationBar extends VBox implements Initializable {
     private static final String COMPONENT_NAME = "NavigationBar";
 
 
-    public NavigationBar() throws ComponentLoaderException {
-//        TODO Make class
+    public NavigationBar() throws ComponentException {
         new FXMLComponentLoader().load(COMPONENT_NAME, this);
 
     }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setVgrow(this, Priority.ALWAYS);
+
+        sceneProperty().addListener((observable, oldScene, newScene) -> {
+            if(newScene != null) {
+                prefHeightProperty().bind(newScene.heightProperty());
+            }
+        });
+    }
+
+
 }
