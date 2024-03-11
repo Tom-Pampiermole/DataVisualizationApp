966d8f10b2b537af0cf293de4007eaf9cd638c5d
tom <tpampiermole@gmail.com>
2024-03-11 11:27:47 +0100

    feat(scene): fix loading via FXML due to invalid path to resource

--- src/main/java/me/tom/visualdata/scene/loader/FXMLSceneLoader.java
+++ src/main/java/me/tom/visualdata/scene/loader/FXMLSceneLoader.java
@@ -1,4 +1,4 @@
-package me.tom.visualdata.datavisualizationapp.scene.loader;
+package me.tom.visualdata.scene.loader;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -9,7 +9,7 @@ public class FXMLSceneLoader implements SceneLoader {
     @Override
     public Scene load(String name, int width, int height) throws SceneLoaderException {
         try {
-            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource(String.format("scene/fxml/%s.fxml", name)));
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(String.format("/scene/fxml/%s.fxml", name)));
             return new Scene(fxmlLoader.load(), width, height);
         } catch(IOException e) {
             throw new SceneLoaderException(e);
