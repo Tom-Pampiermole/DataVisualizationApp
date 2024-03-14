dd811de936a0ed9d3c6991b080b1833d6f7f5d28
tom <tpampiermole@gmail.com>
2024-03-14 08:44:35 +0100

    feat(navigation): add component NavigationBarButton

---
+++ src/main/java/me/tom/visualdata/scene/components/navigation/NavigationBarButton.java
@@ -0,0 +1,23 @@
+package me.tom.visualdata.scene.components.navigation;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import me.tom.visualdata.scene.components.ComponentException;
+import me.tom.visualdata.scene.components.loader.FXMLComponentLoader;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class NavigationBarButton extends Button implements Initializable {
+    private static final String COMPONENT_NAME = "navigation/NavigationBarButton";
+
+
+    public NavigationBarButton() throws ComponentException {
+        new FXMLComponentLoader().load(COMPONENT_NAME, this);
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        setMaxWidth(Integer.MAX_VALUE);
+    }
+}
