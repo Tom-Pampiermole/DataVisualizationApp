c0bae2eda9d0190ea2f3248cb8e7f85d962bd955
tom <tpampiermole@gmail.com>
2024-03-14 08:45:22 +0100

    test(component): add assertion to ComponentUtil#bindPrefSizeToActiveSceneOfRegion

--- src/main/java/me/tom/visualdata/scene/components/ComponentUtil.java
+++ src/main/java/me/tom/visualdata/scene/components/ComponentUtil.java
@@ -13,6 +13,7 @@ public class ComponentUtil {
      * @param region {@link Region} to bind its' size properties of
      */
     public static void bindPrefSizeToActiveSceneOfRegion(Region region) {
+        assert region != null : "Cannot bind preferred size of region to active scene: region null";
         region.sceneProperty().addListener((observable, oldScene, newScene) -> {
             if(newScene != null) {
                 region.prefHeightProperty().bind(newScene.heightProperty());
