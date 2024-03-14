8100588589828db6ef5128233c5034ab3ce8a1ba
tom <tpampiermole@gmail.com>
2024-03-14 08:43:06 +0100

    feat(component): add ComponentUtil#bindPrefSizeToActiveSceneOfRegion

---
+++ src/main/java/me/tom/visualdata/scene/components/ComponentUtil.java
@@ -0,0 +1,24 @@
+package me.tom.visualdata.scene.components;
+
+import javafx.scene.layout.Region;
+
+public class ComponentUtil {
+    private ComponentUtil() { }
+
+
+    /**
+     *  Binds the preferred size properties of given {@link Region} to the size of parent scene to be updated when the
+     *  parent scene changes.
+     *
+     * @param region {@link Region} to bind its' size properties of
+     */
+    public static void bindPrefSizeToActiveSceneOfRegion(Region region) {
+        region.sceneProperty().addListener((observable, oldScene, newScene) -> {
+            if(newScene != null) {
+                region.prefHeightProperty().bind(newScene.heightProperty());
+                region.prefWidthProperty().bind(newScene.widthProperty().divide(5));
+            }
+        });
+    }
+
+}
