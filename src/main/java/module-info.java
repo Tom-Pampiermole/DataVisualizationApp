ca85e1119878413df206c2ab334521ff46cfefa2
tom <tpampiermole@gmail.com>
2024-03-22 20:01:24 +0100

    feat(navigation-bar): add to module exports

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module me.tom.visualdata.datavisualizationapp {
 
 
     opens me.tom.visualdata to javafx.fxml;
+    opens me.tom.visualdata.scene.components.navigation to javafx.fxml;
     opens scene.fxml to javafx.fxml;
     opens scene.fxml.components.navigation to javafx.fxml;
     exports me.tom.visualdata;
