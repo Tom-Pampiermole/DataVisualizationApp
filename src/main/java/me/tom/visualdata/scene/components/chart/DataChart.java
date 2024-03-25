4f98e4c24cd30e8561dba53d4b4660cde44e3026
tom <tpampiermole@gmail.com>
2024-03-25 13:18:02 +0100

    feat(data-chart): let DataChart.java load component from FXML

--- src/main/java/me/tom/visualdata/scene/components/chart/DataChart.java
+++ src/main/java/me/tom/visualdata/scene/components/chart/DataChart.java
@@ -1,8 +1,17 @@
 package me.tom.visualdata.scene.components.chart;
 
 import javafx.scene.chart.Chart;
+import me.tom.visualdata.scene.components.ComponentException;
+import me.tom.visualdata.scene.components.loader.FXMLComponentLoader;
 
 public class DataChart extends Chart {
+    private static final String COMPONENT_NAME = "chart/DataChart";
+
+
+    public DataChart() throws ComponentException {
+        new FXMLComponentLoader().load(COMPONENT_NAME, this);
+
+    }
     @Override
     protected void layoutChartChildren(double v, double v1, double v2, double v3) {
 
