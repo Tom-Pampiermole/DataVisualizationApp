4b73f850787a568eb0a3324539454dc9b65c8d49
tom <tpampiermole@gmail.com>
2024-03-25 13:14:21 +0100

    feat(data-chart): add class and FXML page for data chart

---
+++ src/main/java/me/tom/visualdata/scene/components/chart/DataChart.java
@@ -0,0 +1,10 @@
+package me.tom.visualdata.scene.components.chart;
+
+import javafx.scene.chart.Chart;
+
+public class DataChart extends Chart {
+    @Override
+    protected void layoutChartChildren(double v, double v1, double v2, double v3) {
+
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,9 +5,14 @@ module me.tom.visualdata.datavisualizationapp {
 
     opens me.tom.visualdata to javafx.fxml;
     opens me.tom.visualdata.scene.components.navigation to javafx.fxml;
+    opens me.tom.visualdata.scene.components.chart to javafx.fxml;
+
     opens scene.fxml to javafx.fxml;
     opens scene.fxml.components.navigation to javafx.fxml;
+
+
     exports me.tom.visualdata;
     exports me.tom.visualdata.scene.components;
     exports me.tom.visualdata.scene.components.navigation;
+    exports me.tom.visualdata.scene.components.chart;
 }
