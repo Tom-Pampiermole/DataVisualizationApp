5b1142bfae3e6f9dbaa2e4115b4a28f593564a02
tom <tpampiermole@gmail.com>
2024-04-21 17:00:19 +0200

    fix(data-table): load proper component in DataTable.java

--- src/main/java/me/tom/visualdata/scene/components/data/DataTable.java
+++ src/main/java/me/tom/visualdata/scene/components/data/DataTable.java
@@ -5,7 +5,7 @@ import me.tom.visualdata.scene.components.loader.ComponentLoaderException;
 import me.tom.visualdata.scene.components.loader.FXMLComponentLoader;
 
 public class DataTable extends TableView<String> {
-    private static final String COMPONENT_NAME = "chart/PieDataChart";
+    private static final String COMPONENT_NAME = "data/DataTable";
 
     public DataTable() throws ComponentLoaderException {
         new FXMLComponentLoader().load(COMPONENT_NAME, this);
