aa06172324d9a3014ed56e6843a82f9afd2bced2
tom <tpampiermole@gmail.com>
2024-04-21 18:46:37 +0200

    feat(pom): add dependency on apache POI for reading files in Excel format

--- src/main/java/me/tom/visualdata/scene/components/chart/PieDataChart.java
+++ src/main/java/me/tom/visualdata/scene/components/chart/PieDataChart.java
@@ -1,5 +1,8 @@
 package me.tom.visualdata.scene.components.chart;
 
+import javafx.beans.binding.Bindings;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.scene.chart.PieChart;
 import me.tom.visualdata.scene.components.ComponentException;
 import me.tom.visualdata.scene.components.loader.FXMLComponentLoader;
@@ -8,9 +11,17 @@ public class PieDataChart extends PieChart {
     private static final String COMPONENT_NAME = "chart/PieDataChart";
 
 
-    public PieDataChart() throws ComponentException {
+    private final ObservableList<PieChart.Data> pieChartData;
+
+    public PieDataChart() throws ComponentException, InterruptedException {
         new FXMLComponentLoader().load(COMPONENT_NAME, this);
 
+        pieChartData = FXCollections.observableArrayList();
+
+        dataProperty().bind(Bindings.createObjectBinding(() -> pieChartData, pieChartData));
     }
+
+
+    // Method to add data
 
 }
