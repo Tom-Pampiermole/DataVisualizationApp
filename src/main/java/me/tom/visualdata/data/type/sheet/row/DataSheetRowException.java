e65a092cfe48bf253594b5774d86a796d7f3bcac
tom <tpampiermole@gmail.com>
2024-04-22 14:14:35 +0200

    feat(data-sheet): add DataSheetRowException.java

---
+++ src/main/java/me/tom/visualdata/data/type/sheet/row/DataSheetRowException.java
@@ -0,0 +1,9 @@
+package me.tom.visualdata.data.type.sheet.row;
+
+import me.tom.visualdata.data.type.sheet.DataSheetException;
+
+public class DataSheetRowException extends DataSheetException {
+    protected DataSheetRowException(String message) {
+        super(message);
+    }
+}
