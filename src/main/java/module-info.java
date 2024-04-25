ba1c6a180a8c488c84604527dec9490fca82b04b
tom <tpampiermole@gmail.com>
2024-04-25 12:26:35 +0200

    feat(data-sheet): add 'org.apache.poi.ooxml' as required to module-info.java and export '.scene.components.data'

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,13 @@
 module me.tom.visualdata.datavisualizationapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.apache.poi.ooxml;
 
 
     opens me.tom.visualdata to javafx.fxml;
     opens me.tom.visualdata.scene.components.navigation to javafx.fxml;
     opens me.tom.visualdata.scene.components.chart to javafx.fxml;
+    opens me.tom.visualdata.scene.components.data to javafx.fxml;
 
     opens scene.fxml to javafx.fxml;
     opens scene.fxml.components.navigation to javafx.fxml;
