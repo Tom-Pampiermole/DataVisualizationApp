9eb7a2047bc5d55c4a9c54e513e60e7bdc4630e6
tom <tpampiermole@gmail.com>
2024-05-01 11:54:36 +0200

    feat(data-sheet): add DataSheetLoaderException.java to be thrown by any DataSheetLoader.java

---
+++ src/main/java/me/tom/visualdata/data/type/sheet/loader/DataSheetLoaderException.java
@@ -0,0 +1,9 @@
+package me.tom.visualdata.data.type.sheet.loader;
+
+import me.tom.visualdata.data.type.sheet.DataSheetException;
+
+public class DataSheetLoaderException extends DataSheetException {
+    protected DataSheetLoaderException(String message) {
+        super(message);
+    }
+}
