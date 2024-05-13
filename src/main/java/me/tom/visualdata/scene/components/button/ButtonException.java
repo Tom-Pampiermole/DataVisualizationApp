30b59d4d465a9744e1899c35ebe41327776ff733
tom <tpampiermole@gmail.com>
2024-05-13 15:20:10 +0200

    feat(component-button): add ButtonException.java

---
+++ src/main/java/me/tom/visualdata/scene/components/button/ButtonException.java
@@ -0,0 +1,9 @@
+package me.tom.visualdata.scene.components.button;
+
+import me.tom.visualdata.scene.components.ComponentException;
+
+public class ButtonException extends ComponentException {
+    protected ButtonException(String message) {
+        super(message);
+    }
+}
