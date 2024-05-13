d740dac63020391a1fe23fc5094cd467274f47aa
tom <tpampiermole@gmail.com>
2024-05-13 15:40:30 +0200

    feat(component-data): make DataTable.java editable

--- src/main/java/me/tom/visualdata/scene/components/data/DataTable.java
+++ src/main/java/me/tom/visualdata/scene/components/data/DataTable.java
@@ -5,17 +5,21 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.TextFieldTableCell;
 import me.tom.visualdata.data.type.table.SheetDataTableLoader;
-import me.tom.visualdata.scene.components.loader.ComponentLoaderException;
+import me.tom.visualdata.scene.components.ComponentException;
 import me.tom.visualdata.scene.components.loader.FXMLComponentLoader;
 
 import java.net.URL;
 
 public class DataTable extends TableView<ObservableList<String>> {
     private static final String COMPONENT_NAME = "data/DataTable";
+    private static final boolean EDITABLE = true;
 
-    public DataTable() throws ComponentLoaderException {
+
+    public DataTable() throws ComponentException {
         new FXMLComponentLoader().load(COMPONENT_NAME, this);
+        setEditable(EDITABLE);
 
         URL urlOfFile = getClass().getClassLoader().getResource("dummydata.xlsx");
         new SheetDataTableLoader(urlOfFile.getPath()).load(this);
@@ -26,8 +30,23 @@ public class DataTable extends TableView<ObservableList<String>> {
         TableColumn<ObservableList<String>, String> newColumn = new TableColumn<>(name);
         newColumn.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue().size() > columnIndex ? param.getValue().get(columnIndex) : ""));
         getColumns().add(newColumn);
+        if(EDITABLE) {
+            makeColumnEditable(newColumn);
+        }
 
         addEmptyCellsInColumnForExistentRows();
+    }
+
+    /**
+     *  Makes given {@link TableColumn} editable
+     *
+     * @param column {@link TableColumn} to be made editable
+     */
+    private void makeColumnEditable(TableColumn<ObservableList<String>, String> column) {
+        assert EDITABLE : "Failed to make column editable: table not editable";
+        assert column != null : "Failed to make column editable: column null";
+        column.setCellFactory(TextFieldTableCell.forTableColumn());
+
     }
 
     /**
